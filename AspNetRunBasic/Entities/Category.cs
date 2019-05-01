96306ba02536f0dcc9a17cf3b24b5c00005aea57
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-05-01 20:17:55 +0300

    Category entity added

---
+++ AspNetRunBasic/Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetRunBasic.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(80)]
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+    }
+}
--- AspNetRunBasic/Entities/Product.cs
+++ AspNetRunBasic/Entities/Product.cs
@@ -11,6 +11,12 @@ namespace AspNetRunBasic.Entities
 
         [Required, StringLength(255)]
         public string Description { get; set; }
+
+        public int UnitPrice { get; set; }
+
         public ProductStatus Status { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }

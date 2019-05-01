296c95f720225935a30610eaae31cd8fecdc8cd8
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-05-01 13:24:15 +0300

    AspnetRunContext added

--- AspNetRunBasic/Data/AspnetRunContext.cs
+++ AspNetRunBasic/Data/AspnetRunContext.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using AspNetRunBasic.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AspNetRunBasic.Data
 {
-    public class AspnetRunContext
+    public class AspnetRunContext : DbContext
     {
+        public AspnetRunContext(DbContextOptions<AspnetRunContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
     }
 }

0080da6856bf0c7582c0cf8d18cf884355dae282
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-05-01 14:10:49 +0300

    GetProductByNameAsync

---
+++ AspNetRunBasic/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using AspNetRunBasic.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AspNetRunBasic.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductListAsync();
+        Task<IEnumerable<Product>> GetProductByNameAsync(string name);
+        Task<IEnumerable<Product>> GetProductByCategoryAsync(int categoryId);
+    }
+}
---
+++ AspNetRunBasic/Repositories/ProductRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetRunBasic.Data;
+using AspNetRunBasic.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetRunBasic.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        protected readonly AspnetRunContext _dbContext;
+
+        public ProductRepository(AspnetRunContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task<IEnumerable<Product>> GetProductListAsync()
+        {
+            return await _dbContext.Products.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByNameAsync(string name)
+        {
+            return await _dbContext.Products.Where(p => p.Name.Contains(name)).ToListAsync();            
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByCategoryAsync(int categoryId)
+        {
+            throw new NotImplementedException();            
+        }
+            
+    }
+}

82dae106bad893c1f3daae620853f9c9453fe605
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-05-01 13:31:30 +0300

    Product and ProductStatus entities created

--- AspNetRunBasic/Entities/Product.cs
+++ AspNetRunBasic/Entities/Product.cs
@@ -1,14 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace AspNetRunBasic.Entities
 {
     public class Product
     {
         public int Id { get; set; }
+
+        [Required, StringLength(80)]
         public string Name { get; set; }
+
+        [Required, StringLength(255)]
         public string Description { get; set; }
+        public ProductStatus Status { get; set; }
+
     }
 }

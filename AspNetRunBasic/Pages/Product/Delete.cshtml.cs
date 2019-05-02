a2eed6f226029b37ac8d89a1ed95703269e9286a
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-05-02 10:01:19 +0300

    DeleteModel

---
+++ AspNetRunBasic/Pages/Product/Delete.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetRunBasic.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetRunBasic.Pages.Product
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly IProductRepository _productRepository;
+
+        public DeleteModel(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+        }
+
+        [BindProperty]
+        public Entities.Product Product { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? productId)
+        {
+            if (productId == null)
+            {
+                return NotFound();
+            }
+
+            Product = await _productRepository.GetProductByIdAsync(productId.Value);
+            if (Product == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? productId)
+        {
+            if (productId == null)
+            {
+                return NotFound();
+            }
+
+            await _productRepository.DeleteAsync(Product);
+            return RedirectToPage("./Index");
+        }
+    }
+}

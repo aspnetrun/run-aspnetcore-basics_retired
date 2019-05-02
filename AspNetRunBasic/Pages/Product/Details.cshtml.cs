3028419b9cc62bb525197234227b03c72e3575f6
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-05-02 10:06:26 +0300

    DetailsModel added

---
+++ AspNetRunBasic/Pages/Product/Details.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetRunBasic.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetRunBasic.Pages.Product
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly IProductRepository _productRepository;
+
+        public DetailsModel(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+        }
+
+        public Entities.Product Product { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? productId)
+        {
+            if (productId == null)
+            {
+                return NotFound();
+            }
+
+            Product = await _productRepository.GetProductByIdAsync(productId.Value);
+            if (Product == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+        
+    }
+}

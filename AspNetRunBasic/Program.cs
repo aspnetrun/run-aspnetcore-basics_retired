d88428f6c1028415ef6c20d0b294bf18e291b35d
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-05-04 21:10:13 +0300

    refactor

--- AspNetRunBasic/Program.cs
+++ AspNetRunBasic/Program.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AspNetRunBasic.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
